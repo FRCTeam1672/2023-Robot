f25ae142a222ea679937df4054c2aea8f80b4565
notemmu <emilieinzinna@gmail.com>
2023-02-17 17:54:05 -0500

    begin sysid

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,7 +17,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
-  private RobotContainer m_robotContainer;
+  private RobotContainer robotContainer;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -27,7 +27,7 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+    robotContainer = new RobotContainer();
   }
 
   /**
@@ -44,6 +44,7 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    robotContainer.updatePDPVolt();
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
@@ -56,12 +57,6 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
   }
 
   /** This function is called periodically during autonomous. */
---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,45 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveSubsystem extends SubsystemBase {
+    private final WPI_TalonSRX rightFrontDriveMotor = new WPI_TalonSRX(2);
+    private final WPI_TalonSRX leftFrontDriveMotor = new WPI_TalonSRX(3);
+    private final WPI_TalonSRX backRightDriveMotor = new WPI_TalonSRX(4);
+    private final WPI_TalonSRX backLeftDriveMotor = new WPI_TalonSRX(5);
+
+    private final DifferentialDrive drive;
+
+    private final XboxController xboxController;
+
+    public DriveSubsystem() {
+        this.rightFrontDriveMotor.setInverted(true);
+        this.backRightDriveMotor.setInverted(true);
+
+        this.backLeftDriveMotor.follow(this.leftFrontDriveMotor);
+        this.backRightDriveMotor.follow(this.rightFrontDriveMotor);
+
+        this.leftFrontDriveMotor.setNeutralMode(NeutralMode.Brake);
+        this.rightFrontDriveMotor.setNeutralMode(NeutralMode.Brake);
+        this.backLeftDriveMotor.setNeutralMode(NeutralMode.Brake);
+        this.backRightDriveMotor.setNeutralMode(NeutralMode.Brake);
+
+        this.drive = new DifferentialDrive(leftFrontDriveMotor, rightFrontDriveMotor);
+        this.xboxController = new XboxController(0);
+    }
+    
+    @Override
+    public void periodic() {
+        //grab controller X and Y vales
+        //pass to DifferentialDrive arcadedrive (x foward, y rotate)
+        double xSpeed = -xboxController.getLeftY();
+        double zRotation = -xboxController.getRightX();
+        
+        drive.arcadeDrive(xSpeed, zRotation);
+    }
+}
