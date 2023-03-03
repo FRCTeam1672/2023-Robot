634437186d008689804898a4d525e43b6a5d869d
notemmu <emilieinzinna@gmail.com>
2023-03-03 16:21:03 -0500

    red

--- src/main/java/frc/robot/subsystems/LEDLightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDLightSubsystem.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import java.util.function.Consumer;
+
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -8,16 +10,51 @@ public class LEDLightSubsystem extends SubsystemBase{
     private final AddressableLED addressableLED = new AddressableLED(7);
     private final AddressableLEDBuffer addressableLEDBuffer = new AddressableLEDBuffer(150);
     private int rainbowFirstPixelHue = 0;
+    private LedState currentState = LedState.RED;
 
-    public LEDLightSubsystem(){
+    public enum LedState {
+        RED((ledBuffer) -> {
+            for (int i = 0; i < ledBuffer.getLength(); i++) {
+                ledBuffer.setHSV(i, 0, 255, 128);
+            }
+        }),
+        BLUE((ledBuffer) -> {
+
+        }),
+        PURPLE((ledBuffer) -> {
+            
+        }),
+        YELLOW((ledBuffer) -> {
+            
+        }),
+        REDPULSE((ledBuffer) -> {
+            
+        }),
+        BLUEPULSE((ledBuffer) -> {
+            
+        }),
+        RAINBOW((ledBuffer) -> {
+            
+        }),
+        OFF((ledBuffer) -> {
+            
+        });
+
+        public final Consumer<AddressableLEDBuffer> setBuffer;
+
+        LedState(Consumer<AddressableLEDBuffer> bufferSetter) {
+            setBuffer = bufferSetter;
+        }
+    }
+
+    public LEDLightSubsystem() {
         addressableLED.setLength(addressableLEDBuffer.getLength());
         addressableLED.setData(addressableLEDBuffer);
         addressableLED.start();
     }
-
     @Override
     public void periodic() {
-        rainbow();
+        currentState.setBuffer.accept(addressableLEDBuffer);
         addressableLED.setData(addressableLEDBuffer);
     }
     private void rainbow() {
