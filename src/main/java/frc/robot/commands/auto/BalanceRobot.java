d114f0e5939e20c3f00a6e69f6b948c9e80d7d0b
Kiera <kierajreed@gmail.com>
2023-03-11 09:37:13 -0500

    balance robot revert

--- src/main/java/frc/robot/commands/auto/BalanceRobot.java
+++ src/main/java/frc/robot/commands/auto/BalanceRobot.java
@@ -1,14 +1,19 @@
 package frc.robot.commands.auto;
 
 import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.GyroUtils;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.GyroSubsystem;
 
 public class BalanceRobot extends CommandBase {
-    private final GyroSubsystem gyroSubsystem;
-    private final DriveSubsystem driveSubsystem;
+    private GyroSubsystem gyroSubsystem;
+    private DriveSubsystem driveSubsystem;
+    private int finished = 0;
 
     public BalanceRobot(GyroSubsystem gyroSubsystem, DriveSubsystem driveSubsystem) {
         addRequirements(gyroSubsystem);
@@ -18,8 +23,22 @@ public class BalanceRobot extends CommandBase {
 
     @Override
     public void execute() {
+        SmartDashboard.putBoolean("Balancing", true);
         AHRS ahrs = this.gyroSubsystem.getAHRS();
         double xSpeed = GyroUtils.getRoll(ahrs.getPitch());
+        if(xSpeed == 0.0){
+            SmartDashboard.putBoolean("Balancing Zero", true);
+            finished++;
+        }
+        else{
+            finished = 0;
+            SmartDashboard.putBoolean("Balancing Zero", false);
+        }
         this.driveSubsystem.drive(xSpeed, 0.0);
+    }
+    @Override
+    public void end(boolean interrupted) {
+        SmartDashboard.putBoolean("Balancing", false);
+        SmartDashboard.putBoolean("Balancing Zero", false);
     }
 }
