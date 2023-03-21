b2a094c2660d1c68f37da189e5ae9219b95123c1
Ishaan <bikechamp007@gmail.com>
2023-03-21 16:01:11 -0400

    steal balance from chief

--- src/main/java/frc/robot/commands/auto/BalanceRobot.java
+++ src/main/java/frc/robot/commands/auto/BalanceRobot.java
@@ -1,46 +1,22 @@
 package frc.robot.commands.auto;
 
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.GyroSubsystem;
 
 public class BalanceRobot extends CommandBase {
-    private final GyroSubsystem gyroSubsystem;
-    private final DriveSubsystem driveSubsystem;
-    private Timer stopTimer = new Timer();
+    private ChiefBalance balancer;
+    private DriveSubsystem drive;
 
-    public BalanceRobot(GyroSubsystem gyroSubsystem, DriveSubsystem driveSubsystem) {
-        addRequirements(gyroSubsystem);
-        this.gyroSubsystem = gyroSubsystem;
-        this.driveSubsystem = driveSubsystem;
+    public BalanceRobot(DriveSubsystem drive) {
+        this.balancer = new ChiefBalance();
+        this.drive = drive;
     }
 
-    public void initialize() {
-        SmartDashboard.putBoolean("Fully Balanced", false);
-        stopTimer.reset();
-    }
-
-    @Override
     public void execute() {
-        if(gyroSubsystem.getPitch() < -6) {
-            driveSubsystem.drive(-0.6, 0);
-        } else if(gyroSubsystem.getPitch() > 6) {
-            driveSubsystem.drive(0.55, 0);
-            stopTimer.start();
-        } else {
-            driveSubsystem.drive(0, 0);
-        }
+        drive.drive(balancer.autoBalanceRoutine(), 0);
     }
 
-    public boolean isFinished() {
-        return stopTimer.hasElapsed(1.5);
-    }
-
-    @Override
-    public void end(boolean interrupted) {
-        //SmartDashboard.putBoolean("Attempting Balancing", false);
-        SmartDashboard.putBoolean("Fully Balanced", true);
+    public boolean isFinished(boolean interrupted) {
+        return false;
     }
 }
