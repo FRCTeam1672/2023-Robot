cc5c87c3aaac1cb249674306b9b19004807862a7
Kiera <kierajreed@gmail.com>
2023-03-25 15:43:23 -0400

    charge station changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -138,8 +138,8 @@ public class RobotContainer {
                 .getStowCommand()
                 .andThen(
                     new DriveRobotToChargeStation(driveSubsystem, gyroSubsystem)
-                    .andThen(new TimerCommand(() -> driveSubsystem.drive(-0.785, 0), 0.81))
-                    .andThen(new TimerCommand(() -> driveSubsystem.drive(0, 0.86), 0.6))
+                    .andThen(new TimerCommand(() -> driveSubsystem.drive(-0.785, 0), 0.825))
+                    .andThen(new TimerCommand(() -> driveSubsystem.drive(0, 0.86), 0.55))
                     .andThen(driveSubsystem::stop)
                 );
 
